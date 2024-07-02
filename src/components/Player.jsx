dd9581c3b50eb860b2c6324066a6420d17595a70
Steve Delors Ngounou <stevedelors7@gmail.com>
2024-07-02 08:40:17 +0100

    Project files
    Main components and game logic added

---
+++ src/components/Player.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, SetIsEditing] = useState(false);
+
+  function handleEdit() {
+    SetIsEditing((editing) => !editing);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+
+  function handleSaveName(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleSaveName}
+      />
+    );
+  }
+
+  return (
+    <li className={isActive ? "active" : undefined}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEdit}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
